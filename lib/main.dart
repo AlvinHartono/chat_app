263d73e9828b68e09c40ef38ed6175df1277bf8e
AlvinHartono <128015193+AlvinHartono@users.noreply.github.com>
2023-10-09 15:16:43 +0700

    added sign up logic

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'firebase_options.dart';
 import 'package:chat_app/screen/auth.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
